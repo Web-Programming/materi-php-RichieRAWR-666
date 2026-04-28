1ff8a2e16313a272ae87204c464badd7e17c49b7
RichieRAWR-666 <nathanielrichie30@gmail.com>
2026-04-28 12:57:20 +0700

    commit

---
+++ ricicih/resources/views/app/sidebar.blade.php
@@ -0,0 +1,7 @@
+<h6 class="text-uppercase text-muted mb-3">Menu</h6>
+
+<div class="list-group list-group-flush">
+    <a href="#" class="list-group-item list-group-item-action active">Dashboard</a>
+    <a href="#" class="list-group-item list-group-item-action">Produk</a>
+    <a href="#" class="list-group-item list-group-item-action">Pengaturan</a>
+</div>
--- ricicih/routes/web.php
+++ ricicih/routes/web.php
@@ -57,3 +57,117 @@ Route::get('?test-method/', function (){
 
 
 
+//Route ke halaman utama
+Route::get('/', function () {
+    echo 'Hello, Nama saya Celline';
+    // return view('welcome');
+});
+
+//Route ke halaman alamat
+Route::get('/alamat', function(){
+    echo 'Jalan Rajawali No. 14 Palembang';
+    echo '<br>';
+    echo 'Rt. 01 Rw. 02';
+    echo '<br>';
+    echo 'Kota Palembang';
+    echo '<br>';
+    echo 'Provinsi Sumatera Selatan';
+});
+
+//Route dinamis dengan parameter id
+Route::get('/user/{id}', function($id){
+    echo 'User ID: ' .$id;
+});
+
+//Route dinamis dengan parameter nama
+Route::get('/user2/{name}', function($name){
+    echo 'User Name: ' .$name;
+});
+
+//Route dinamis dengan opsional parameter nama
+Route::get('/user3/{name?}', function($name = 'Tamu'){
+    echo 'User Name: ' .$name;
+});
+
+//Route dinamis dengan parameter nama dan id
+Route::get('/user4/{id}/{name}', function($id, $name){
+    echo 'User ID: ' .$id;
+    echo '<br>';
+    echo 'User Name: ' .$name;
+});
+
+//Router dengan metode POST -> menyimpan
+Route::get('/simpan', function(){
+    echo 'Data berhasil disimpan';
+});
+
+//Router dengan metode PUT -> update data
+Route::get('/update/{id}', function($id){
+    echo 'Data berhasil diperbarui dengan ID: ' .$id;
+});
+
+//Router dengan metode PATCH -> update data
+Route::get('/update2/{id}', function($id){
+    echo 'Data berhasil diperbarui dengan ID: ' .$id;
+});
+
+//Router dengan metode DELETE -> menghapus data
+Route::get('/hapus/{id}', function($id){
+    echo 'Data berhasil dihapus dengan ID: ' .$id;
+});
+
+//Route untuk menampilkan halaman test_method
+Route::get('/test-method', function(){
+    return view('test_method');
+});
+
+//Menampilkan halaman profile
+Route::get('/profile', function(){
+    return view('profile');
+});
+
+//Gunakan . untuk memisahkan folder dengan view
+Route::get('/detailproduk', function(){
+    return view('produk.detail');
+});
+
+//Mengirim data ke view
+Route::get('/detailproduk/{name}', function($name){
+    return view('produk.detail', 
+    ['product_name'=>$name,
+    'id'=> 101,
+    'color'=> 'Silver',
+    'Stock'=> 12
+    ]);
+});
+
+Route::get('/detailprdouk/{name}', function($name){
+    return view('produk.detail',
+        ['product_name'=> $name,
+            'id'=> '$id',
+            'color'=> 'Silver',
+            'Stock'=> 12
+        ]
+    );
+});
+
+Route::get('/produk/', function(){
+    return view('produk.indexa');
+});
+
+Route::get('/produk/', function(){
+    return view('produk.index');
+});
+
+Route::get('/produk/create', function(){
+    return view('produk.create');
+});
+
+Route::get('/produk/search', function(){
+    return view('produk.search');
+});
+
+Route::get('/produk/detail', function(){
+    return view('produk.detail');
+});
+
